37fa80472e8e25afd86f1d5b4ae5536e23a65009
MAXEE998 <hl2752@nyu.edu>
2018-03-25 02:03:30 +0800

    fix faulty response caused by token invalid (may result in too many api request)

--- nyuapi/core.py
+++ nyuapi/core.py
@@ -3,11 +3,10 @@ import time
 
 
 class Core():
-
     TOKEN = ""
     CATE = {
-        "course":   "course-catalog-exp/courses",
-        "class":  "class-roster-exp/classes",
+        "course": "course-catalog-exp/courses",
+        "class": "class-roster-exp/classes",
         "calendar": "academic-calendar-exp/dates",
         "faculty": "faculty-exp"
     }
@@ -23,12 +22,15 @@ class Core():
     def __init__(self, token):
         if not token:
             raise Exception("[Error] Token can not be empty!")
-        
+
         self.TOKEN = token
         self.R = request.reqNYU(self.TOKEN)
 
     def getRawFacultyByNYUId(self, nyu_id):
-        getById = self.R.rawReq(self.CATE["faculty"] + "/users/nyu-ids/" + nyu_id, {})
+        while 1:
+            getById = self.R.rawReq(self.CATE["faculty"] + "/users/nyu-ids/" + nyu_id, {})
+            if isinstance(getById, list):
+                break
         return getById
 
     def getRawFacultyByKeyword(self, keyword):
@@ -37,27 +39,36 @@ class Core():
         for word in keyword:
             while 1:
                 class_list = self.getRawClassesByInstuctor(word)
-                if type(class_list) == type([1]):
-                   break
+                if isinstance(class_list, list):
+                    break
             for session in class_list:
                 instructor_nyuid = session["instructor_nyu_id"]
                 if instructor_nyuid not in rsp:
                     rsp[instructor_nyuid] = dict(instructor_nyu_id=session["instructor_nyu_id"],
-                                                    instructor_role_description=session["instructor_role_description"],
-                                                    instructor_name=session["instructor_name"])
+                                                 instructor_role_description=session["instructor_role_description"],
+                                                 instructor_name=session["instructor_name"])
         rsp = list(rsp.values())
         return rsp
 
     def getRawClassesByInstuctor(self, keyword):
-        getById = self.R.rawReq(self.CATE["class"] + "?instructor_name=" + keyword, {})
+        while 1:
+            getById = self.R.rawReq(self.CATE["class"] + "?instructor_preferred_name=" + keyword, {})
+            if isinstance(getById, list):
+                break
         return getById
 
     def getRawClassesById(self, course_id):
-        getById = self.R.rawReq(self.CATE["class"] + "?course_id=" + course_id, {})
+        while 1:
+            getById = self.R.rawReq(self.CATE["class"] + "?course_id=" + course_id, {})
+            if isinstance(getById, list):
+                break
         return getById
 
     def getRawCourses(self, keywords=""):
-        getByTitle = self.R.rawReq(self.CATE["course"] + "?course_title=" + keywords + "&limit=15", {})
+        while 1:
+            getByTitle = self.R.rawReq(self.CATE["course"] + "?course_title=" + keywords + "&limit=15", {})
+            if isinstance(getByTitle, list):
+                break
         return getByTitle
 
     def getRawCalenders(self, date="", term=None):
@@ -75,6 +86,6 @@ class Core():
             param = "term=" + term + "-" + str(time.localtime()[0])
         else:
             param = "term=" + term
-        
-        getCalenders = self.R.rawReq(self.CATE["calendar"] +"?"+ param, {})
+
+        getCalenders = self.R.rawReq(self.CATE["calendar"] + "?" + param, {})
         return getCalenders
