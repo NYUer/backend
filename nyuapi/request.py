b1221dd7bd609f4bc8341d868a1b6b71c04ea8b1
MAXEE998 <hl2752@nyu.edu>
2018-04-02 12:47:13 +0800

    typo……
    it seems that NYU api has

--- nyuapi/request.py
+++ nyuapi/request.py
@@ -13,24 +13,30 @@ class reqNYU():
         self.ping()
     
     def ping(self):
-        req = R.get("https://sandbox.api.it.nyu.edu/course-catalog-exp/", headers={
-            "Authorization": "Bearer " + self.TOKEN
-        })
+        try:
+            req = R.get("https://sandbox.api.it.nyu.edu/course-catalog-exp/", headers={
+                "Authorization": "Bearer " + self.TOKEN
+            }, timeout=10)
+        except R.exceptions.ReadTimeout:
+            raise Exception("[Error] NYU API not responding!")
         if req.text.find("Invalid or missing token") > -1:
             raise Exception("[Error] Token is not valid!")
 
     def rawReq(self, uri="", params={}):
         print("A request has been sent.")
-        req = R.get(self.BASEURI + uri, data=params, headers={
-            "Authorization": "Bearer " + self.TOKEN
-        })
+        try:
+            req = R.get(self.BASEURI + uri, data=params, headers={
+                "Authorization": "Bearer " + self.TOKEN
+            }, timeout=10)
+        except R.exceptions.ReadTimeout:
+            raise Exception("[Error] NYU API not responding!")
         return req.json()
 
-    def repeatReq(self, uri="", params={}):
+    def repeatReq(self, url="", params={}):
         """
-        server will send request repeatedly until valid reponse is received.
+        server will send request repeatedly until valid response is received.
         However, if token invalid msg keep appearing, the server will halt.
-        Therefore, a server moniter is needed.
+        Therefore, a server monitor is needed.
         """ 
         counter = 0
         while 1:
